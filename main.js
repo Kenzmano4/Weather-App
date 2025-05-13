34a908059bc4c16c4b3a80f20d29524ce984fa43
kenzman04 <Emmanuelkomo@2024>
2025-05-13 17:25:49 +0100

    Update main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+const api = "6cc181966a192a8d1ade729bb5a0130c";
+const api_url = ""
+
+
+const iconImg = document.getElementById("weather-icon");
+const loc = document.getElementById("location");
+const tempC = document.getElementsByClassName("c");
+const tempF = document.getElementsByClassName("f");
+const desc = document.getElementsByClassName("desc");
+const sunriseDOM = document.getElementsByClassName("sunrise");
+
+
